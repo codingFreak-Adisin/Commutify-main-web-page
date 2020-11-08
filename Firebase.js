3d47d8731b9b4d5c952b70dc208b8a486a250d66
Aditya Sinha <70958307+codingFreak-Adisin@users.noreply.github.com>
2020-11-08 15:03:37 +0530

    Add files via upload

---
+++ Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAFAHbQbEAzHu_2zo5fEcl_8BE7hSE1jCU",
+    authDomain: "commutify-redirected-page-hack.firebaseapp.com",
+    databaseURL: "https://commutify-redirected-page-hack.firebaseio.com",
+    projectId: "commutify-redirected-page-hack",
+    storageBucket: "commutify-redirected-page-hack.appspot.com",
+    messagingSenderId: "126422512321",
+    appId: "1:126422512321:web:b4acbd5f3914b68fd15a40",
+    measurementId: "G-QCLSS5RJDM"
+};
